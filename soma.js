8b522dfeb4b1d473fa4645961137418c0fe5a35a
Neal Jackson <nealjack@umich.edu>
2018-05-16 16:04:56 -0700

    made a bit more reliable

--- soma.js
+++ soma.js
@@ -6,24 +6,28 @@ var util   = require('util');
 var noble = require('noble');
 
 //battery
-var BATTERY_SERVICE_UUID = "180f".toLowerCase()
-var BATTERY_CHARACTERISTIC_UUID = "2a19".toLowerCase()
+const BATTERY_SERVICE_UUID = "180f".toLowerCase()
+const BATTERY_CHARACTERISTIC_UUID = "2a19".toLowerCase()
 
 //motor control
-var MOTOR_SERVICE_UUID = "00001861B87F490C92CB11BA5EA5167C".toLowerCase()
-var MOTOR_STATE_CHARACTERISTIC_UUID = "00001525B87F490C92CB11BA5EA5167C".toLowerCase()
-var MOTOR_CONTROL_CHARACTERISTIC_UUID = "00001530B87F490C92CB11BA5EA5167C".toLowerCase()
-var MOTOR_TARGET_CHARACTERISTIC_UUID = "00001526B87F490C92CB11BA5EA5167C".toLowerCase()
-var MOTOR_MOVE_UP = 0x69
-var MOTOR_STOP = 0x50
-var MOTOR_MOVE_DOWN = 0x96
+const MOTOR_SERVICE_UUID = "00001861B87F490C92CB11BA5EA5167C".toLowerCase()
+const MOTOR_STATE_CHARACTERISTIC_UUID = "00001525B87F490C92CB11BA5EA5167C".toLowerCase()
+const MOTOR_CONTROL_CHARACTERISTIC_UUID = "00001530B87F490C92CB11BA5EA5167C".toLowerCase()
+const MOTOR_TARGET_CHARACTERISTIC_UUID = "00001526B87F490C92CB11BA5EA5167C".toLowerCase()
+const MOTOR_MOVE_UP = 0x69
+const MOTOR_STOP = 0x50
+const MOTOR_MOVE_DOWN = 0x96
 
-var soma = function (MACList, ready_callback) {
+const DISCOVER_TIMEOUT = 2000
+
+var soma = function (MACList) {
 
   this._mac_addresses = {};
-  this._current_peripheral = '';
-  this._ready_callback = ready_callback;
+  this._current_peripheral = null;
+  this._current_address    = null;
+  this._ready_callback = null;
   this._num_macs = MACList.length;
+  this._discover_timeout = null;
 
   for(var i = 0; i < MACList.length; i++) {
     this._mac_addresses[MACList[i].toLowerCase()] = {};
@@ -33,20 +37,29 @@ var soma = function (MACList, ready_callback) {
   debug('Creating new SOMA shade object');
 
   noble.on('discover', this.on_discover.bind(this));
-  noble.on('scanChanged', this.on_scan_changed.bind(this));
+  //noble.on('scanChanged', this.on_scan_changed.bind(this));
   noble.on('disconnect', this.on_disconnect.bind(this));
+};
 
-  var startScanningOnPowerOn = function() {
+soma.prototype.connectDevice = function(mac, cb) {
+  if (this._ready_callback == null) {
+    this._ready_callback = cb;
+    this._current_address = mac;
+  }
+  else {
+    cb('Busy');
+  }
+  this.startScanningOnPowerOn();
+  this._discover_timeout = setTimeout(this.readyTimeout.bind(this), DISCOVER_TIMEOUT);
+}
+
+soma.prototype.startScanningOnPowerOn = function() {
     if (noble.state === 'poweredOn') {
       noble.startScanning([], false);
     } else {
-      noble.once('stateChange', startScanningOnPowerOn);
+      noble.once('stateChange', this.startScanningOnPowerOn);
     }
-  };
-
-  startScanningOnPowerOn();
-  setTimeout(this.readyTimeout.bind(this), 20000);
-};
+}
 
 soma.prototype.readyTimeout = function() {
   debug('Called ready timeout');
@@ -59,50 +72,35 @@ soma.prototype.readyTimeout = function() {
   }
 }
 
-// Handle the callback when scanning parameters change out from beneath us.
-// This gives us the ability to set them back to how we want them.
-soma.prototype.on_scan_changed = function (enable, filter_dups) {
-  try {
-    noble.startScanning([], false);
-  } catch (e) { }
-};
+soma.prototype.connect = function() {
+  var that = this;
+  peripheral = this._current_peripheral;
+  peripheral.connect(function (error) {
 
-soma.prototype.on_discover = function (peripheral) {
-  debug(peripheral.address);
-  if(this._mac_addresses[peripheral.address]) {
-    //connect to this peripheral
-    debug('Connecting to ' + peripheral.address);
-    this._mac_addresses[peripheral.address]['peripheral'] = peripheral;
-    this._current_peripheral = peripheral.address;
-    var that = this;
+    if(error) {
+      debug(error);
+    }
 
-    peripheral.once('disconnect',this.on_disconnect.bind(this));
-    peripheral.connect(function (error) {
-      noble.startScanning([], false);
+    if(peripheral.state == 'connected') {
+      peripheral.discoverServices([BATTERY_SERVICE_UUID, MOTOR_SERVICE_UUID], function(error, services) {
+        if(error) {
+          debug(error);
+        }
 
-      if(error) {
-        debug(error);
-      }
+        if(services.length != 2) {
+          debug('Wrong number of services');
+        }
 
-      if(peripheral.state == 'connected') {
-        peripheral.discoverServices([BATTERY_SERVICE_UUID, MOTOR_SERVICE_UUID], function(error, services) {
-          if(error) {
-            debug(error);
-          }
-
-          if(services.length != 2) {
-            debug('Wrong number of services');
-          }
+        if(peripheral.state == 'connected') {
+          services[0].discoverCharacteristics([BATTERY_CHARACTERISTIC_UUID], function(error,chars) {
+            if(error) {
+              noble.startScanning([], false);
+              debug(error);
+            }
+            that._mac_addresses[peripheral.address].battery_char = chars[0];
+          });
 
           if(peripheral.state == 'connected') {
-            services[0].discoverCharacteristics([BATTERY_CHARACTERISTIC_UUID], function(error,chars) {
-              if(error) {
-                noble.startScanning([], false);
-                debug(error);
-              }
-              that._mac_addresses[peripheral.address].battery_char = chars[0];
-            });
-
             services[1].discoverCharacteristics([MOTOR_STATE_CHARACTERISTIC_UUID,MOTOR_CONTROL_CHARACTERISTIC_UUID,MOTOR_TARGET_CHARACTERISTIC_UUID], function(error,chars) {
               if(error) {
                 debug(error);
@@ -112,117 +110,159 @@ soma.prototype.on_discover = function (peripheral) {
               that._mac_addresses[peripheral.address].motor_target_char = chars[1];
               that._mac_addresses[peripheral.address].motor_control_char = chars[2];
 
-              var discovered = 0;
-              for(var periph in noble._peripherals) {
-                if(noble._peripherals[periph].services != null) {
-                  discovered += 1;
-                }
-              }
-              if(discovered >= that._num_macs) {
-                if(that._ready_callback != null) {
-                  debug('Calling ready callback from discovery');
-                  debug(that);
-                  that._ready_callback();
-                  that._ready_callback = null;
-                }
+              if(that._ready_callback != null) {
+                debug('Calling ready callback from discovery');
+                //debug(that);
+                that._ready_callback(null);
+                that._ready_callback = null;
+                that._current_peripheral = null;
+                that._current_address    = null;
               }
             });
+          } else return;
+          debug('Finished service discovery for ' +  peripheral.address);
+        } else return;
+      });
+    } else return;
+  });
+}
 
-            debug('Finished service discovery for ' +  peripheral.address);
-          }
+soma.prototype.on_discover = function (peripheral) {
+  if(this._current_address === peripheral.address && this._current_peripheral == null) {
+    noble.stopScanning();
+    //connect to this peripheral
+    debug('Connecting to ' + peripheral.address);
+    this._mac_addresses[peripheral.address]['peripheral'] = peripheral;
+    this._current_peripheral = peripheral;
+    var that = this;
 
-        });
-      }
-    });
+    peripheral.once('disconnect',this.on_disconnect.bind(this));
+    this.connect();
   }
-};
+}
+
+//// Handle the callback when scanning parameters change out from beneath us.
+//// This gives us the ability to set them back to how we want them.
+//soma.prototype.on_scan_changed = function (enable, filter_dups) {
+//  try {
+//    noble.startScanning([], false);
+//  } catch (e) { }
+//};
 
 soma.prototype.on_disconnect = function(error) {
   debug('Disconnected');
+  if (this._current_peripheral != null && this._ready_callback != null) {
+    return;
+  }
 };
 
 soma.prototype.get_battery = function(mac, callback) {
-  if(!this._mac_addresses[mac.toLowerCase()].battery_char) {
-    callback('DoesNotExist', null);
-  }
-
-  this._mac_addresses[mac.toLowerCase()].battery_char.read(function(error, data) {
+  var that = this;
+  this.connectDevice(mac.toLowerCase(), function(error) {
     if(error) {
       debug(error)
     }
+    if(!that._mac_addresses[mac.toLowerCase()].battery_char) {
+      callback('DoesNotExist', null);
+    }
+    that._mac_addresses[mac.toLowerCase()].battery_char.read(function(error, data) {
+      if(error) {
+        debug(error)
+      }
 
-    callback(error, data[0]);
+      callback(error, data[0]);
+      that._mac_addresses[mac.toLowerCase()]['peripheral'].disconnect();
+    });
   });
-
 }
 
 soma.prototype.get_position = function(mac, callback) {
-  if(!this._mac_addresses[mac.toLowerCase()].motor_state_char) {
-    callback('DoesNotExist', null);
-  }
-
-  this._mac_addresses[mac.toLowerCase()].motor_state_char.read(function(error, data) {
-    if(error) {
-      debug(error)
+  var that = this;
+  this.connectDevice(mac.toLowerCase(), function(error) {
+    if(!that._mac_addresses[mac.toLowerCase()].motor_state_char) {
+      callback('DoesNotExist', null);
     }
 
-    callback(error, data[0]);
+    that._mac_addresses[mac.toLowerCase()].motor_state_char.read(function(error, data) {
+      if(error) {
+        debug(error)
+      }
+
+      callback(error, data[0]);
+      that._mac_addresses[mac.toLowerCase()]['peripheral'].disconnect();
+    });
   });
 }
 
 soma.prototype.set_position = function(mac, position, callback) {
-  if(!this._mac_addresses[mac.toLowerCase()].motor_target_char) {
-    callback('DoesNotExist');
-  }
-
-  this._mac_addresses[mac.toLowerCase()].motor_target_char.write(new Buffer([position]), false, function(error) {
-    if(error) {
-      debug(error)
+  var that = this;
+  this.connectDevice(mac.toLowerCase(), function(error) {
+    if(!that._mac_addresses[mac.toLowerCase()].motor_target_char) {
+      callback('DoesNotExist');
     }
 
-    callback(error);
+    that._mac_addresses[mac.toLowerCase()].motor_target_char.write(new Buffer([position]), false, function(error) {
+      if(error) {
+        debug(error)
+      }
+
+      callback(error);
+      that._mac_addresses[mac.toLowerCase()]['peripheral'].disconnect();
+    });
   });
 }
 
 soma.prototype.move_up = function(mac, callback) {
-  if(!this._mac_addresses[mac.toLowerCase()].motor_control_char) {
-    callback('DoesNotExist');
-  }
-
-  this._mac_addresses[mac.toLowerCase()].motor_control_char.write(new Buffer([MOTOR_MOVE_UP]), false, function(error) {
-    if(error) {
-      debug(error)
+  var that = this;
+  this.connectDevice(mac.toLowerCase(), function(error) {
+    if(!that._mac_addresses[mac.toLowerCase()].motor_control_char) {
+      callback('DoesNotExist');
     }
 
-    callback(error);
+    that._mac_addresses[mac.toLowerCase()].motor_control_char.write(new Buffer([MOTOR_MOVE_UP]), false, function(error) {
+      if(error) {
+        debug(error)
+      }
+
+      callback(error);
+      that._mac_addresses[mac.toLowerCase()]['peripheral'].disconnect();
+    });
   });
 }
 
 soma.prototype.move_down = function(mac, callback) {
-  if(!this._mac_addresses[mac.toLowerCase()].motor_control_char) {
-    callback('DoesNotExist');
-  }
-
-  this._mac_addresses[mac.toLowerCase()].motor_control_char.write(new Buffer([MOTOR_MOVE_DOWN]), false, function(error) {
-    if(error) {
-      debug(error)
+  var that = this;
+  this.connectDevice(mac.toLowerCase(), function(error) {
+    if(!that._mac_addresses[mac.toLowerCase()].motor_control_char) {
+      callback('DoesNotExist');
     }
 
-    callback(error);
+    that._mac_addresses[mac.toLowerCase()].motor_control_char.write(new Buffer([MOTOR_MOVE_DOWN]), false, function(error) {
+      if(error) {
+        debug(error)
+      }
+
+      callback(error);
+      that._mac_addresses[mac.toLowerCase()]['peripheral'].disconnect();
+    });
   });
 }
 
 soma.prototype.stop = function(mac, callback) {
-  if(!this._mac_addresses[mac.toLowerCase()].motor_control_char) {
-    callback('DoesNotExist');
-  }
-
-  this._mac_addresses[mac.toLowerCase()].motor_control_char.write(new Buffer([MOTOR_STOP]), false, function(error) {
-    if(error) {
-      debug(error)
+  var that = this;
+  this.connectDevice(mac.toLowerCase(), function(error) {
+    if(!that._mac_addresses[mac.toLowerCase()].motor_control_char) {
+      callback('DoesNotExist');
     }
 
-    callback(error);
+    that._mac_addresses[mac.toLowerCase()].motor_control_char.write(new Buffer([MOTOR_STOP]), false, function(error) {
+      if(error) {
+        debug(error)
+      }
+
+      callback(error);
+      that._mac_addresses[mac.toLowerCase()]['peripheral'].disconnect();
+    });
   });
 }
 
@@ -244,80 +284,73 @@ if (require.main === module) {
     .argv;
 
   var mac = argv.m;
-  shades = new soma([mac], function(error) {
-    debug('Returned');
-    if(error) {
-      console.log('Error: ' + error);
-      process.exit(1)
-    }
-
-    if(argv._[0] == 'battery') {
-      shades.get_battery(mac, function(error, data) {
-        if(error) {
-          console.log('Error: ' + error);
-          process.exit(1)
-        }
-
-        console.log('Battery level: ' + data);
-        process.exit(0)
-      });
-    } else if(argv._[0] == 'position') {
-      shades.get_position(mac, function(error, data) {
-        if(error) {
-          console.log('Error: ' + error);
-          process.exit(1)
-        }
-
-        console.log('Position: ' + data);
-        process.exit(0)
-      });
-    } else if(argv._[0] == 'target') {
-      if(argv._.length < 2) {
-        console.log('Error: must provide position argument with target');
+  shades = new soma([mac]);
+  if(argv._[0] == 'battery') {
+    shades.get_battery(mac, function(error, data) {
+      if(error) {
+        console.log('Error: ' + error);
         process.exit(1)
       }
 
-      shades.set_position(mac, argv._[1], function(error) {
-        if(error) {
-          console.log('Error: ' + error);
-          process.exit(1)
-        } else {
-          console.log('Successfully set target');
-          process.exit(0)
-        }
-      });
-    } else if(argv._[0] == 'up') {
-      shades.move_up(mac, function(error) {
-        if(error) {
-          console.log('Error: ' + error);
-          process.exit(1)
-        } else {
-          console.log('Moving up');
-          process.exit(0)
-        }
-      });
-    } else if(argv._[0] == 'down') {
-      shades.move_down(mac, function(error) {
-        if(error) {
-          console.log('Error: ' + error);
-          process.exit(1)
-        } else {
-          console.log('Moving down');
-          process.exit(0)
-        }
-      });
-    } else if(argv._[0] == 'stop') {
-      shades.stop(mac, function(error) {
-        if(error) {
-          console.log('Error: ' + error);
-          process.exit(1)
-        } else {
-          console.log('Stopping');
-          process.exit(0)
-        }
-      });
+      console.log('Battery level: ' + data);
+      process.exit(0)
+    });
+  } else if(argv._[0] == 'position') {
+    shades.get_position(mac, function(error, data) {
+      if(error) {
+        console.log('Error: ' + error);
+        process.exit(1)
+      }
+
+      console.log('Position: ' + data);
+      process.exit(0)
+    });
+  } else if(argv._[0] == 'target') {
+    if(argv._.length < 2) {
+      console.log('Error: must provide position argument with target');
+      process.exit(1)
     }
-  });
+
+    shades.set_position(mac, argv._[1], function(error) {
+      if(error) {
+        console.log('Error: ' + error);
+        process.exit(1)
+      } else {
+        console.log('Successfully set target');
+        process.exit(0)
+      }
+    });
+  } else if(argv._[0] == 'up') {
+    shades.move_up(mac, function(error) {
+      if(error) {
+        console.log('Error: ' + error);
+        process.exit(1)
+      } else {
+        console.log('Moving up');
+        process.exit(0)
+      }
+    });
+  } else if(argv._[0] == 'down') {
+    shades.move_down(mac, function(error) {
+      if(error) {
+        console.log('Error: ' + error);
+        process.exit(1)
+      } else {
+        console.log('Moving down');
+        process.exit(0)
+      }
+    });
+  } else if(argv._[0] == 'stop') {
+    shades.stop(mac, function(error) {
+      if(error) {
+        console.log('Error: ' + error);
+        process.exit(1)
+      } else {
+        console.log('Stopping');
+        process.exit(0)
+      }
+    });
+  }
 } else {
   module.exports = soma;
 }
